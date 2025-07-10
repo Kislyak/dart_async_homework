527859dbd237228e77a7fc900f157510a2e77dd8
Kysliak Andrii <Kislyak.work@gmail.com>
2025-07-10 18:56:44 +0300

    feat: task_2-async receiving age

---
+++ lib/homeworks/lesson_11/task_2.dart
@@ -0,0 +1,27 @@
+Future<String> fetchAge() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  return '25';
+}
+
+Future<String> getAgeString() async {
+  final String age = await fetchAge();
+  final String formattedAge = formatAge(int.parse(age));
+  return formattedAge;
+}
+
+String formatAge(int age) {
+  int lastDigit = age % 10;
+  int lastTwoDigits = age % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+    return '$age років';
+  }
+
+  if (lastDigit == 1) {
+    return '$age рік';
+  } else if (lastDigit >= 2 && lastDigit <= 4) {
+    return '$age роки';
+  } else {
+    return '$age років';
+  }
+}
