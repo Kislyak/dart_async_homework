ba0a2bc8022af02a037f6fc442efbc0a3b8652fb
Kysliak Andrii <Kislyak.work@gmail.com>
2025-07-14 17:14:01 +0300

    feat: task_3 sequantial Future execution

--- lib/hw_futures.dart
+++ lib/hw_futures.dart
@@ -1,7 +1,22 @@
+import 'package:dart_async_homework/homeworks/lesson_11/task_1.dart';
 import 'package:dart_async_homework/homeworks/lesson_11/task_2.dart';
 
-void main() {
-  getAgeString().then((ageString) {
-    print('Мені $ageString');
+void main() async {
+  final Stopwatch stopwatch = Stopwatch();
+  stopwatch.start();
+
+  await fetchName().then((name) {
+    print('Мене звати $name');
   });
+
+  stopwatch.stop();
+  print('Час виконання запиту на ім\'я: ${stopwatch.elapsedMilliseconds} мс');
+
+  stopwatch.reset();
+  stopwatch.start();
+  final ageString = await getAgeString();
+  print('Мені $ageString');
+
+  stopwatch.stop();
+  print('Час виконання запиту на вік: ${stopwatch.elapsedMilliseconds} мс');
 }
